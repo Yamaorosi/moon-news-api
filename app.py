48e4f99c34394673f5c4740abe0d436b43683093
Yamaorosi <k862160@kansai-u.ac.jp>
2026-05-04 18:44:59 +0900

    Update app.py
    にゅーすAPIをつくる

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def health():
-    return {"status": "ok"}
+    return jsonify({"status": "ok"})
+
+@app.route("/news")
+def news():
+    return jsonify({
+        "fact": [],
+        "structure": [],
+        "sensation": []
+    })
