504f90acc5fda71100273216018b7f968a86650f
Yamaorosi <k862160@kansai-u.ac.jp>
2026-05-04 16:47:19 +0900

    Change route to /healthz for health check
    パスを対応させる

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return {"status": "ok", "message": "moon-news-api is running"}
+@app.route("/healthz")
+def health():
+    return {"status": "ok"}
