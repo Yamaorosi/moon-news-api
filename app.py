a69cb266b3c617979a3e2c598bd7536bbbf858d5
Yamaorosi <k862160@kansai-u.ac.jp>
2026-05-05 00:10:41 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,44 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+import requests
+import os
 
 app = Flask(__name__)
+
+API_KEY = os.getenv("NEWS_API_KEY")
+BASE_URL = "https://newsapi.org/v2/top-headlines"
+
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
+
 @app.route("/status")
 def status():
     return jsonify({"status": "ok"})
 
+
 @app.route("/news")
 def news():
-   return jsonify({
-    "fact": "米国株が上昇",
-    "structure": "AI期待と地政学",
-    "sensation": "都市の光が揺れる"
-})
+
+    params = {
+        "country": "us",
+        "category": "technology",
+        "pageSize": 3,
+        "apiKey": API_KEY
+    }
+
+    response = requests.get(BASE_URL, params=params)
+    articles = response.json().get("articles", [])
+
+    result = [
+        {
+            "title": a["title"],
+            "source": a["source"]["name"],
+            "url": a["url"],
+            "publishedAt": a["publishedAt"]
+        }
+        for a in articles
+    ]
+
+    return jsonify(result)
