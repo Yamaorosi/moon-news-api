231963de3a336d51a880e31ff8b8bb9441c242ee
Yamaorosi <k862160@kansai-u.ac.jp>
2026-05-05 09:24:31 +0900

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def status():
 def news():
 
     params = {
-        "country": "ja",
+        "country": "us",
         "category": "technology",
         "pageSize": 3,
         "apiKey": API_KEY
