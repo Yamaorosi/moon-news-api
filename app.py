5ef292c9206a8f4f51a118979e420988062938ce
Yamaorosi <k862160@kansai-u.ac.jp>
2026-05-05 10:14:46 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,50 +1,22 @@
+# app.py
 from flask import Flask, render_template, jsonify, request
-import requests
-import os
+from services import fetch_tech_news  # 作業員を呼び出す
 
 app = Flask(__name__)
-
-API_KEY = os.getenv("NEWS_API_KEY")
-print("API_KEY:", API_KEY)
-if not API_KEY:
-    print("❌ API_KEYが設定されてない")
-BASE_URL = "https://newsapi.org/v2/top-headlines"
-
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-
 @app.route("/status")
 def status():
     return jsonify({"status": "ok"})
 
-
 @app.route("/news")
 def news():
-
-    params = {
-        "country": "jp",
-        "category": "technology",
-        "pageSize": 3,
-        "apiKey": API_KEY
-    }
-
-    response = requests.get(BASE_URL, params=params)
-    articles = response.json().get("articles", [])
-
-    result = [
-        {
-            "title": a["title"],
-            "source": a["source"]["name"],
-            "url": a["url"],
-            "publishedAt": a["publishedAt"]
-        }
-        for a in articles
-    ]
-
-    return jsonify(result)
+    # 実際の作業は職人（service）に任せて、結果をJSONで返すだけ
+    data = fetch_tech_news()
+    return jsonify(data)
 
 @app.route("/article")
 def article():
