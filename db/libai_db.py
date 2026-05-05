c44aa3d5f5a247ce3660870fa5d28355d908a1dc
Yamaorosi <k862160@kansai-u.ac.jp>
2026-05-05 16:55:50 +0900

    Create libai_db.py

---
+++ db/libai_db.py
@@ -0,0 +1,23 @@
+import sqlite3
+import os
+
+BASE_DIR = os.path.dirname(__file__)
+DB_PATH = os.path.join(BASE_DIR, "libai.db")
+
+def get_libai_conn():
+    return sqlite3.connect(DB_PATH)
+
+def init_libai_db():
+    conn = get_libai_conn()
+    cur = conn.cursor()
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS poems (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT,
+        text TEXT,
+        dynasty TEXT,
+        theme TEXT
+    )
+    """)
+    conn.commit()
+    conn.close()
