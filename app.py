f24e3830bf288b7b88fa9a2563d48cbe871905eb
Yamaorosi <k862160@kansai-u.ac.jp>
2026-05-06 13:06:18 +0900

    debug news

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # app.py
 from flask import Flask, render_template, jsonify, request
 from services import fetch_tech_news, get_all_poems,fetch_and_store_news
-from db.news_db import init_news_db
+from db.news_db import init_news_db,get_news_conn
 from db.libai_db import init_libai_db
 import os
 
@@ -68,6 +68,24 @@ def update_news():
         print("ERROR:", e)
         return {"status": "error", "message": str(e)}, 500
 
+
+        
+@app.route("/debug/news")
+def debug_news():
+    conn = get_news_conn()
+    cur = conn.cursor()
+
+    rows = cur.execute("""
+        SELECT id, title, body, source, url, created_at
+        FROM news
+        ORDER BY id DESC
+    """).fetchall()
+
+    conn.close()
+
+    return [
+        dict(r) for r in rows
+    ]
 
 # --- app.run は必ず一番最後に書く！ ---
 if __name__ == "__main__":
